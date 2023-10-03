0a1aca5a2bb094b975e95c490e4e9cd66a48f9dd
William Stevenson <williamstevenson107@gmail.com>
2023-10-02 22:04:58 -0500

    Added the start to a sign-in page and began working on safe password storage

---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,65 @@
+import Popup from 'reactjs-popup';
+import { useState } from 'react';
+import 'reactjs-popup/dist/index.css';
+import { useNavigate, useLocation } from "react-router-dom";
+
+import axios from 'axios'
+import "./SignIn.scss"
+const keytar= require('keytar')
+const port = process.env.PORT || 4234
+
+function SignIn() {
+    const [open, setOpen] = useState(false);
+    const location = useLocation()
+    const navigate = useNavigate()
+    const loggedIn = false
+    
+
+    function signUp(){
+        
+    }
+
+    function popupHandler(e) {
+        e.preventDefault()
+      
+        
+
+    }
+    function signinHandler(e) {
+        e.preventDefault()
+        keytar.getPassword('hands-off', e.target.username.value)
+            .then((data) => {
+                if (data.response === e.target.password) {
+                    loggedIn(true)
+                    navigate('/post')
+
+                }
+                alert('Wrong, try again')
+                return
+            })
+    }
+    return (
+        <>
+            <form className="sign-in-form" onSubmit={(e) => { signinHandler(e) }}>
+                <input className="sign-in-form__username" name="username" required placeholder="username"></input>
+                <input type="password" className="sign-in-form__password" name="password" required placeholder="password"></input>
+                <button type="submit" placeholder="Login" className="sign-in-form__login">Login</button>
+            </form>
+            <button placeholder="Sign-up?" onClick={(e) => {
+                e.preventDefault()
+                setOpen(true)
+            }} className={"signup-button"}>Sign-Up?</button>
+            <Popup open={open} position="center center" closeOnDocumentClick={false} className="signup_popup">
+                <form className="signup__popup" onSubmit={(e) => { popupHandler(e) }}>
+                    <button onClick={() => setOpen(false)}>Close</button>
+                    <input type="text" name="account" placeholder='username' required></input>
+                    <input type="password" name="password" placeholder='password' required></input>
+                    <input type='file' name="saveFile" placeholder='Where do you want to save your profile?' className="signup__popup--saveDir" directory="" webkitdirectory="" multiple></input>
+                    <input type="submit" placeholder="Create Profile"></input>
+                </form>
+            </Popup>
+
+        </>
+    )
+}
+export default SignIn;
