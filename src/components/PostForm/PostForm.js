1ea715ff796d107098be90c190b0112c5f653086
Mason Stevenson <williamstevenson107@gmail.com>
2023-11-11 23:45:17 -0600

    implemented jwt and completed profile creation as well as sign up functionality

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -4,64 +4,70 @@ import Popup from 'reactjs-popup';
 import { useState, useEffect } from 'react';
 import 'reactjs-popup/dist/index.css';
 import axios from 'axios';
-import dotenv from "dotenv"
-dotenv.config()
+import { useNavigate, useLocation } from "react-router-dom"
+import bcrypt from "bcryptjs"
+
 
 /*  */
 
 const obs = new OBSWebSocket();
 
 function PostForm() {
+    const navigate = useNavigate();
+    const location = useLocation()
+    const state = location.state
+    
+    const sessionState= sessionStorage.getItem('oauth2-state')
+    console.log(sessionState)
+    console.log(state)
+    const [params, setParams] = useState(JSON.parse(process.env.REACT_APP_PARAMS));
+    params['state'] = state;
     const [open, setOpen] = useState(false);
     const [recordingFolder, setRecordingFolder] = useState();
-    const [fileName, setFileName] = useState()
-    const [params, setParams]=useState([])
+    const [fileName, setFileName] = useState();
     const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
-    const [store, setStore]=useState([]);
+    const [store, setStore] = useState([]);
+
+    console.log(state)
     ////////OAuth////////////////////////////////////////////////////////////////////
     // Google's OAuth 2.0 endpoint for requesting an access token
-
-
     useEffect(() => {
-        axios.post('http://localhost:3000/api/auth',undefined, {headers: {
-
-        'Accept': 'application/json'
-    }})
-            .then((response) => {
-                console.log(response.data)
-                params.push(response.data)
-                setParams(params[0])
-                setStore(params)
-                console.log(params)
-                const fragmentString = window.location.hash.substring(1);
-                // Parse query string to see if page request is coming from OAuth 2.0 server.
-                let regex = /([^&=]+)=([^&]*)/g, m;
-                while (m = regex.exec(fragmentString)) {
-                    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
-                }
-                if (Object.keys(params).length > 0) {
-                    localStorage.setItem('oauth2-test-params', JSON.stringify(params));
-                    if (params['state'] && params['state'] == 'try_sample_request') {
-                        trySampleRequest();
-                    }
-                }
-
-            }).catch((err) => {
-                console.log(err+": error getting params")
-            })
+        setStore(params)
+        const fragmentString = window.location.hash.substring(1);
+        // Parse query string to see if page request is coming from OAuth 2.0 server.
+        let regex = /([^&=]+)=([^&]*)/g, m;
+        while (m = regex.exec(fragmentString)) {
+            params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+        }
+        if (Object.keys(params).length > 0) {
+            localStorage.setItem('oauth2-test-params', JSON.stringify(params));
+            if (params['state'] && params['state'] == 'try_sample_request') {
+                trySampleRequest();
+            }
+        }
         // Parameters to pass to OAuth 2.0 endpoint.
-
-
     }, [])
+    async function verifyState(stateToCheck) {
+        
+        if (stateToCheck) {
+            const sessionState= sessionStorage.getItem('oauth2-state')
+            console.log(sessionState)
+            return await bcrypt.compare(stateToCheck, sessionState).then((response) => {
+                return response
+            })
+        }
+    }
 
-
-
-    function trySampleRequest(e) {
+    async function trySampleRequest(e) {
         setParams(JSON.parse(localStorage.getItem('oauth2-test-params')));
+        setStore(params)
+        console.log(store)
         console.log(params)
-        if (params && params['access_token']) {
+        console.log(state)
+        const stateIsValid = verifyState(params['state'])
+        if (params && params['access_token'] && stateIsValid==='true') {
             const xhr = new XMLHttpRequest();
-            xhr.open('GET',
+            xhr.open('POST',
                 'https://www.googleapis.com/youtube/v3/channels?part=snippet&mine=true&' +
                 'access_token=' + params['access_token']);
             xhr.onreadystatechange = function () {
@@ -79,7 +85,6 @@ function PostForm() {
     }
 
     function oauth2SignIn(e) {
-
         for (let p in params) {
             const input = document.createElement('input');
             input.setAttribute('type', 'hidden');
@@ -87,13 +92,13 @@ function PostForm() {
             input.setAttribute('value', params[p]);
             e.target.appendChild(input);
         }
-
-        e.target.submit()
+        e.target.submit();
 
     }
+
     ////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////
+    /////Function to post the video to Youtube//////////////////////////////////////////////
     async function popupHandler(e) {
         e.preventDefault()
         const snippetData = {
@@ -101,24 +106,21 @@ function PostForm() {
             description: e.target['video-description'].value,
             tags: ['tag1', 'tag2'],
             categoryId: '22'
-            
+
         }
-       
         setParams(store)
-        await axios.post(`http://localhost:3000/api`, { recordingFolder, params, snippetData}, {
+        await axios.post(`http://localhost:3000/api`, { recordingFolder, params, snippetData }, {
             headers: {
-               'Authorization': `Bearer ${store['access_token']}`,
+                'Authorization': `Bearer ${store['access_token']}`,
                 'Content-Type': 'application/json'
             }
-        
-    }).then((response) => {
+        }).then((response) => {
             console.log(response)
 
 
         }).catch((err) => { console.log(`${err}: error sending folder/video to server or to Youtube`) })
 
     }
-
     ///////////////////////////////////////////////////////////////////////////
     ////////////OBS////////////////////////////////////////////////////////////
     async function OBS(port, url, password) {
@@ -129,7 +131,7 @@ function PostForm() {
             alert("error connecting to OBS-Websocket Server");
         });
         const recordingFolder = await obs.call('GetRecordDirectory')
-       
+
 
 
         await obs.on('StreamStateChanged', (data) => {
@@ -158,12 +160,12 @@ function PostForm() {
             <input type='text' name='server-password' placeholder="password" required></input>
             <button type='submit' className="PostForm__OBS--submit">Connect To OBS</button>
         </form>
-        <form method="GET" action={oauth2Endpoint} onClick={(event) => trySampleRequest(event)}>
+        <form method="POST" action={oauth2Endpoint} onClick={(event) => trySampleRequest(event)}>
             <button type="submit" className="YT-sign-in" >Sign in to YT</button>
         </form>
 
         <Popup open={open} position="center center" closeOnDocumentClick={false}>
-            <form className="postForm__popup" onSubmit={popupHandler}>
+            <form className="postForm__popup" onSubmit={(event) => popupHandler(event)}>
                 <button onClick={() => setOpen(false)}>Close</button>
                 <input type="text" name="video-title" placeholder='The title of your video'></input>
                 <input type="text" name="video-description" placeholder='The description of your video'></input>
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -5,33 +5,54 @@ import { useNavigate, useLocation } from "react-router-dom";
 
 import axios from 'axios'
 import "./SignIn.scss"
-
+import bcrypt from 'bcryptjs'
 
 
 function SignIn() {
+    const state = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)+ Math.random().toString(36).slice(2)+ Math.random().toString(36).slice(2);
+   
     const [open, setOpen] = useState(false);
     const location = useLocation()
     const navigate = useNavigate()
     const loggedIn = false
-    
+   
 
-    
     function popupHandler(e) {
         e.preventDefault()
         axios.post('http://localhost:3000/api/profile',{
             username: e.target['username'].value,
             password: e.target['password'].value
         }).then((response)=>{
+            console.log(response)
             alert('Profile Created')
         }).catch((err)=>{
             console.log(err+" error creating profile.")
         })
-      
-        
-
     }
+    async function hashState() {
+           return await bcrypt.hash(state.toString(), 16).then((response) => {
+                const hash=response
+                sessionStorage.setItem('oauth2-state', hash);
+                return hash
+            });
+        }
     function signinHandler(e) {
         e.preventDefault()
+        
+        axios.post('http://localhost:3000/api/auth',{
+            username: e.target['username'].value,
+            password: e.target['password'].value
+        }).then((response)=>{
+            console.log(response)
+            alert('Successfully signed-in')
+            return hashState()
+        }).then((response)=>{
+                return navigate('/post', {state: response})
+            })
+        .catch((err)=>{
+            console.log(err+" error logging in")
+            alert('Username or password is incorrect')
+        })
        
     }
     return (
@@ -42,15 +63,14 @@ function SignIn() {
                 <button type="submit" placeholder="Login" className="sign-in-form__login">Login</button>
             </form>
             <button placeholder="Sign-up?" onClick={(e) => {
-                e.preventDefault()
+            
                 setOpen(true)
             }} className={"signup-button"}>Sign-Up?</button>
             <Popup open={open} position="center center" closeOnDocumentClick={false} className="signup_popup">
                 <form className="signup__popup" onSubmit={(e) => { popupHandler(e) }}>
                     <button onClick={() => setOpen(false)}>Close</button>
-                    <input type="text" name="account" placeholder='username' required></input>
+                    <input type="text" name="username" placeholder='username' required></input>
                     <input type="password" name="password" placeholder='password' required></input>
-                    <input type='file' name="saveFile" placeholder='Where do you want to save your profile?' className="signup__popup--saveDir" directory="" webkitdirectory="" multiple></input>
                     <input type="submit" placeholder="Create Profile"></input>
                 </form>
             </Popup>
